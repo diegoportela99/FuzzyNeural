6eed6f42c316743c17f1a391e65d150cee72624b
diegoportela99 <diego.f.portela@student.uts.edu.au>
2020-05-14 16:50:04 +1000

    added back-propagation

--- A2part1.m
+++ A2part1.m
@@ -52,6 +52,7 @@ n = 0.25; %learning constant
 for a = (1:C)
    for b = (1:6)
        training_data = x(b,:)';
+       desired = d(b,:)';
        
        %2 neurons from first layer
        first_neuron_weight = weight_vector_to_hidden(1,:)';
@@ -75,14 +76,53 @@ for a = (1:C)
        second_output_neuron_weights = weight_vector_to_output(2,:)';
        third_output_neuron_weights = weight_vector_to_output(3,:)';
        
+       tied_up_output_weights = [
+           first_output_neuron_weights; second_output_neuron_weights; third_output_neuron_weights;
+       ];
+       
        %matrix multiplication output
        first_output_vector = first_output_neuron_weights'*tied_up_activation;
        second_output_vector = second_output_neuron_weights'*tied_up_activation;
        third_output_vector = third_output_neuron_weights'*tied_up_activation;
        
+       tied_up_output_matrix = [
+           first_output_vector; second_output_vector; third_output_vector;
+       ];
+       
        %activation function output
        first_output_activation = (1/(1+exp(-first_output_vector)));
        second_output_activation = (1/(1+exp(-second_output_vector)));
        third_output_activation = (1/(1+exp(-third_output_vector)));
+       
+       %output / desired
+       first_desired_neuron = desired(1) - first_output_activation
+       second_desired_neuron = desired(2) - second_output_activation
+       third_desired_neuron = desired(3) - third_output_activation
+       
+       %back-propagation output layer
+       first_function_derivative_output = exp(first_output_vector)/(exp(first_output_vector)+1)^2;
+       second_function_derivative_output = exp(first_output_vector)/(exp(first_output_vector)+1)^2;
+       third_function_derivative_output = exp(first_output_vector)/(exp(first_output_vector)+1)^2;
+       
+       %output error signal
+       first_error_signal_output = first_desired_neuron*first_function_derivative_output;
+       second_error_signal_output = second_desired_neuron*second_function_derivative_output;
+       third_error_signal_output = third_desired_neuron*third_function_derivative_output;
+       
+       tied_up_output_error_signal = [
+           first_error_signal_output; second_error_signal_output; third_error_signal_output;
+       ];
+   
+       %hidden layer derivative function
+       first_function_derivative_hidden = exp(first_vector)/(exp(first_vector)+1)^2;
+       second_function_derivative_hidden = exp(second_vector)/(exp(second_vector)+1)^2;
+       third_function_derivative_hidden = exp(-1)/(exp(-1)+1)^2;
+       
+       %hidden error signal
+       first_error_signal_hidden = tied_up_output_weights(1)*tied_up_output_error_signal*first_function_derivative_hidden;
+       second_error_signal_hidden = tied_up_output_weights(2)*tied_up_output_error_signal*second_function_derivative_hidden;
+       %3rd neuron is bias function
+       third_error_signal_hidden = tied_up_output_weights(3)*tied_up_output_error_signal*third_function_derivative_hidden;
+       
    end
 end
