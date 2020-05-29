574f172b0b37daa139b5a21360689946f3cfe726
diegoportela99 <diego.f.portela@student.uts.edu.au>
2020-05-29 23:44:38 +1000

    CNN changes

--- A2part2.m
+++ A2part2.m
@@ -1,16 +1,18 @@
-imds = imageDatastore('C:\Users\diego\OneDrive - UTS\UTS\Year 4\Sem 1\Neural Net - fuss logic\A2\images', ...
+gpuDevice(1)
+imds = imageDatastore('C:\Users\diego\OneDrive - UTS\UTS\Year 4\Sem 1\Neural Net - fuss logic\A2\dogs_vs_cats', ...
     'IncludeSubfolders',true, ...
     'LabelSource','foldernames'); % load the images and labels
-[imdsTrain,imdsValidation] = splitEachLabel(imds,0.5   ); %split training and 
-%validation datasets, training is 60%
+
+imds.ReadFcn = @customReadDatastoreImage;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,0.5); %split training and 
+%validation datasets
 
 layers = [
-    imageInputLayer([32 32 3])
+    imageInputLayer([64 64 3])
     convolution2dLayer(5,24,'Stride',1,'Padding',2)
     batchNormalizationLayer
     reluLayer
     maxPooling2dLayer(2,'Stride',2)% the pooling kernel size is 2*2, stride is 2
-    
     
     convolution2dLayer(5,28,'Stride',1,'Padding',2)
     batchNormalizationLayer
@@ -33,21 +35,25 @@ layers = [
  %  'InitialLearnRate',0.001, ...
  %'InitialLearnRate',0.001, 'Momentum',0.4, ...
  options = trainingOptions('adam', ...
-     'ExecutionEnvironment','gpu',...
+     'ExecutionEnvironment','auto',...
      'InitialLearnRate',0.001, ...
      'MaxEpochs',50, ...
-     'minibatch',6, ...
+     'MiniBatch',50, ...
      'ValidationData',imdsValidation, ...
+     'shuffle', 'every-epoch', ...
      'ValidationFrequency',10, ...
      'Plots','training-progress');
  net = trainNetwork(imdsTrain,layers,options);
- net.divideparam.trainRatio = 70/100;    % Training ratio
- net.divideparam.valRatio = 30/100;  % Validation ratio
- net.divideparam.testRatio = 30/100; % Testing Ratio
- 
  net.Layers  % display all the layers of this structure
     
     
-    
-    
-    
+function data = customReadDatastoreImage(filename)
+    % code from default function: 
+    onState = warning('off', 'backtrace'); 
+    c = onCleanup(@() warning(onState)); 
+    reading = imread(filename); % added lines: 
+    %turn_gray = rgb2gray(reading);
+    data = imsharpen(reading); %sharpen the image
+    data = data(:,:,min(1:3, end)); 
+    data = imresize(data,[64 64]);
+end
